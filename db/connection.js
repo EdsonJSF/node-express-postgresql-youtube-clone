b7d696576197981fd2971502250e64712aaf4735
EdsonJSF <EdsonJSF1990@gmail.com>
2023-12-07 16:45:14 -0400

    Feat: Postgresql configuration

--- db/connection.js
+++ db/connection.js
@@ -1,1 +1,13 @@
-import "dotenv/config";
+import pkg from "pg";
+const { Pool } = pkg;
+
+export const pool = new Pool({
+  allowExitOnIdle: true,
+});
+
+try {
+  await pool.query("SELECT NOW()");
+  console.log("Database connected");
+} catch (error) {
+  console.log(error);
+}
